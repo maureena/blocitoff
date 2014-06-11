e44f9b1d7c229269a6edea066094fe38b424a1ff
vagrant <vagrant@rails-dev-box>
2014-06-11 01:36:48 +0000

    First commit and README update

---
+++ README.md
@@ -0,0 +1,3 @@
+Blocitoff: A sef-destructing to-do list.
+
+Made with my mentor at [Bloc](http://bloc.io)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocitoff::Application.config.secret_key_base = '63789239a3c1cd15a1a239b975b993300f77a53f58a42611750848f87de4ce367c4efc55284ae05aeb737220b0692450c9b2d82d91f3f882f4b07a2036915e46'
